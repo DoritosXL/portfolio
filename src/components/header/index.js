0d77a97a953fb47cdea25f8b16227a520cddf35c
Hakan Taskirmaz <doritosvslays@gmail.com>
2019-05-23 01:04:39 +0200

    aanpassingen aan header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,13 +6,16 @@ function Header() {
     <div className="Header">
       <div className="header-container">
 
-        <div id="Over-mij">
-          <a id="a" href="">Over mij</a>
+        <div id="persona">
+          <a id="a" href="">Persona</a>
         </div>
-        <div id="CV">
+        <div id="projecten">
+          <a id="a" href="">Projecten</a>
+        </div>
+        <div id="cv">
           <a id="a" href="">CV</a>
         </div>
-        <div id="Contact">
+        <div id="contact">
           <a id="a" href="">Contact</a>
         </div>
 
