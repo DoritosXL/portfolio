28d85f586d18b5a00b7e7f2d88bd06757e527269
Hakan Taskirmaz <doritosvslays@gmail.com>
2019-06-22 22:13:43 +0200

    moved the navigation to index.js to try and solve the refresh issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Header from './components/header/index.js'
-import Personalia from './components/personalia/index.js';
-import Contact from './components/contact/index.js'
 import Body from './components/body/index.js';
-import Footer from './components/footer/index.js'
-import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+
 
 // CV
 import cv from '../src/components/documents/CV-Hakan-Taskirmaz.pdf';
@@ -23,52 +19,8 @@ class App extends Component {
   render() {
     initializeReactGA()
     return (
-      <div id="container">
-        <Router>
-          <div className="header-container">
-            <div id="projecten">
-              <NavLink className="koppen" to="/" exact activeStyle={
-                { color: 'white' }
-              }>Projecten</NavLink>
-            </div>
-            <div id="personalia">
-              <NavLink className="koppen" to="/personalia" exact activeStyle={
-                { color: 'white' }
-              }>Personalia</NavLink>
-            </div>
-            <div id="contact">
-              <NavLink className="koppen" to="/contact" exact activeStyle={
-                { color: 'white' }
-              }>Contact</NavLink>
-            </div>
-            <div id="cv">
-              <a className="koppen" id="a" href={cv} target="_blank" rel="noopener noreferrer">CV</a>
-            </div>
-          </div>
-
-          <div className="content">
-            <Route path="/personalia" exact strict render={
-              () => {
-                return (<Personalia />);
-              }
-            } />
-            <Route path="/contact" exact strict render={
-              () => {
-                return (<Contact />);
-              }
-            } />
-
-            <Route path="/" exact strict render={
-              () => {
-                return (<Body />);
-              }
-            } />
-          </div>
-        </Router>
-        <footer id="footer">
-            
-         <Footer/>
-        </footer>
+      <div>
+        <Body />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, NavLink, Switch, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Personalia from './components/personalia';
 import Body from './components/body';
+import Contact from './components/contact';
+import cv from '../src/components/documents/CV-Hakan-Taskirmaz.pdf';
+import Footer from './components/footer/index.js'
 
-const routing =(
+const routing = (
   <Router>
-    <div>
-      <Route path="/" component={App} />
-      <Route path="/personalia" component={Personalia} />
-      <Route path="/body" component={Body} />
+    <div id="container">
+      <div className="header-container">
+        <div id="projecten">
+          <NavLink className="koppen" to="/" exact activeStyle={
+            { color: 'white' }
+          }>Projecten</NavLink>
+        </div>
+        <div id="personalia">
+          <NavLink className="koppen" to="/personalia" exact activeStyle={
+            { color: 'white' }
+          }>Personalia</NavLink>
+        </div>
+        <div id="contact">
+          <NavLink className="koppen" to="/contact" exact activeStyle={
+            { color: 'white' }
+          }>Contact</NavLink>
+        </div>
+        <div id="cv">
+          <a className="koppen" id="a" href={cv} target="_blank" rel="noopener noreferrer">CV</a>
+        </div>
+      </div>
+      <div className="content">
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/personalia" component={Personalia} />
+          {/* <Route path="/vaardigheden" component={skills} /> */}
+          <Route path="/contact" component={Contact} />
+        </Switch>
+
+      </div>
+
+      <div id="footer">
+        <Footer />
+      </div>
+
     </div>
   </Router>
 )
