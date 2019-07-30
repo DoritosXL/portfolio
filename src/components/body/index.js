cd69fd8293abd7718b055e58994b501f2e5bda96
Hakan Taskirmaz <hakantaskirmaz@MacBook-Pro-van-Hakan.local>
2019-07-30 21:44:30 +0200

    added new project kapsalon asya

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -9,10 +9,14 @@ import discord_img from '../img/projects/discord.png'
 
 import ML_img from '../img/projects/ML.png'
 
+import kapsalon_asya_img from '../img/projects/About.png'
+
 const project_D = `Project D gaat over 'Machine Learning'. In dit project is gebruik gemaakt van LSTM (Long short-term Memory), 
                   om een voorspelling te doen op de toekomstige voorraad. `;
 
 const discord_bot = `Een discord bot gemaakt om aan te tonen hoe simpel het is om er één te maken. Opzet en basis functies zijn gerealiseerd en te vinden op Github.`;
+
+const kapsalon_asya = `Kapsalon Asya is een kappers zaak te Leyweg in Den Haag. De website is gemaakt met Wordpress en te vinden op kapsalonasya.nl`;
 
 class Body extends React.Component {
   render() {
@@ -38,6 +42,13 @@ class Body extends React.Component {
                 </a>}
               description={discord_bot} />
 
+
+            <Project title={
+              <a id="link" href="https://kapsalonasya.nl/" target="_blank" rel="noopener noreferrer">
+                <img id="project_img" src={kapsalon_asya_img} alt="Kapsalon Asya"></img>
+              </a>
+            } description={kapsalon_asya} />
+            
             {/* Nieuwe project
             <Project title={} description={} />*/}
           </div>
--- src/components/personalia/index.js
+++ src/components/personalia/index.js
@@ -5,16 +5,27 @@ import hakan from '../img/hakan.jpg';
 class Hakan extends React.Component {
   render() {
     return (
-      <div id="body">
+      <div id="personalia">
+        <div id="body">
 
-        <div id="headshot">
-          <img id="headshot_img" alt="hakan" src={hakan} height="auto" />
+
+
+          <div id="headshot">
+            <img id="headshot_img" alt="hakan" src={hakan} height="auto" />
+          </div>
+
+
+
+          <div id="headshot-beschrijving">
+            Mijn naam is Hakan Taskirmaz.
         </div>
 
-        <div id="headshot-beschrijving">
-          Mijn naam is Hakan Taskirmaz.
-        </div>
-      </div >
+
+        </div >
+        {/* <div id="beschrijving">
+          Momenteel studeer ik Informatica op Hogeschool Rotterdam.
+        </div> */}
+      </div>
     );
   }
 }
