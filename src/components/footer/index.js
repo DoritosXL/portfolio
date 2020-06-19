142e93ab6115b2e183f533234ebc03db4db0d6c4
Hakan Taşkirmaz <hakan.taskirmaz@minbzk.nl>
2020-06-19 16:45:56 +0200

    updated email link and CV

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,7 +9,7 @@ function Footer() {
     <div className="Footer">
       <a href="https://www.linkedin.com/in/hakan-taskirmaz-1920b845" target="_blank" rel="noopener noreferrer"><img id="footerimg" alt="Linkedin" src={linkedin} /></a>
       <a href="https://github.com/doritosxl" target="_blank" rel="noopener noreferrer"><img id="footerimg" alt="github" src={github} /></a>
-      <a href="mailto:hi@hakan.life" target="_blank" rel="noopener noreferrer"><img id="footerimg" alt="email" src={email} /></a>
+      <a href="mailto:hi@hakanportfol.io" target="_blank" rel="noopener noreferrer"><img id="footerimg" alt="email" src={email} /></a>
     </div>
   );
 }
