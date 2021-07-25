d837415538ac41f0d62ae7388d2f9ab15e95b064
Hakan Taşkirmaz <hakan.taskirmaz@minbzk.nl>
2021-07-25 19:04:18 +0200

    added two projects: fitness pall and ha pictures

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,30 +1,36 @@
-import React, { Suspense } from "react";
-import "./projects.css";
-import { Helmet } from "react-helmet";
-import Project from "../project/index.js";
-import CircularProgress from '@material-ui/core/CircularProgress';
-import stageLink from '../documents/UBRijk-artikel-Hakan.pdf';
+import React, { Suspense } from "react"
+import "./projects.css"
+import { Helmet } from "react-helmet"
+import Project from "../project/index.js"
+import CircularProgress from "@material-ui/core/CircularProgress"
+import stageLink from "../documents/UBRijk-artikel-Hakan.pdf"
 
-import discord_img from "../img/projects/discord.png";
+import discord_img from "../img/projects/discord.png"
 
-import ML_img from "../img/projects/ML.png";
+import ML_img from "../img/projects/ML.png"
 
-import kapsalon_asya_img from "../img/projects/About.png";
+import kapsalon_asya_img from "../img/projects/About.png"
 
-import project_C_img from "../img/projects/Artic.svg";
+import project_C_img from "../img/projects/Artic.svg"
 
-import stage_img from "../img/projects/stage.svg";
+import stage_img from "../img/projects/stage.svg"
 
-const stage = `Tijdens stage bij SSC-ICT heb ik gewerkt aan een front-end web applicatie gemaakt in ReactJS. Klik op de afbeelding voor meer informatie over mijn stageperiode.`;
+import base_logo from "../img/favicon.ico"
 
-const project_C = `Project C gaat over 'een React webshop'. Dit project is gebouwd met React en imiteert het uiterlijk van een autentieke webshop.`;
+const stage = `Tijdens stage bij SSC-ICT heb ik gewerkt aan een front-end web applicatie gemaakt in ReactJS. Klik op de afbeelding voor meer informatie over mijn stageperiode.`
+
+const project_C = `Project C gaat over 'een React webshop'. Dit project is gebouwd met React en imiteert het uiterlijk van een autentieke webshop.`
 
 const project_D = `Project D gaat over 'Machine Learning'. In dit project is gebruik gemaakt van LSTM (Long short-term Memory), 
-                  om een voorspelling te doen op de toekomstige voorraad. `;
+                  om een voorspelling te doen op de toekomstige voorraad. `
 
-const discord_bot = `Een discord bot gemaakt om aan te tonen hoe simpel het is om er één te maken. Opzet en basis functies zijn gerealiseerd en te vinden op Github.`;
+const discord_bot = `Een discord bot gemaakt om aan te tonen hoe simpel het is om er één te maken. Opzet en basis functies zijn gerealiseerd en te vinden op Github.`
 
-const kapsalon_asya = `Kapsalon Asya is een kappers zaak te Leyweg in Den Haag. De website is gemaakt met Wordpress en te vinden op`;
+const kapsalon_asya = `Kapsalon Asya is een kappers zaak te Leyweg in Den Haag. De website is gemaakt met Wordpress en te vinden op`
+
+const fitness_pall = `Fitness-pall is een web app om nieuwe records met betrekking tot oefeningen aan te geven. Progressie van oefeningen is vervolgens aangegeven in een grafiek. Het project is aangemaakt om zo simpel mogelijk oefeningen door te geven en data terug krijgen.`
+
+const ha_pictures = `Ha-pictures is een web app met inlogsysteem voor het individueel uploaden en inzien van afbeeldingen. Het project is aangemaakt naar aanleiding van mijn interesse in fotografie.`
 
 class Projects extends React.Component {
   render() {
@@ -42,9 +48,18 @@ class Projects extends React.Component {
           <div id="project_container">
             <Project
               title={
-                <a id="link" href={stageLink} target="_blank" rel="noopener noreferrer">
-                  <Suspense fallback={<CircularProgress/>}>
-                  <img id="project_img" src={stage_img} alt="Stage MinBZK"></img>
+                <a
+                  id="link"
+                  href={stageLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Suspense fallback={<CircularProgress />}>
+                    <img
+                      id="project_img"
+                      src={stage_img}
+                      alt="Stage MinBZK"
+                    ></img>
                   </Suspense>
                 </a>
               }
@@ -60,8 +75,8 @@ class Projects extends React.Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <Suspense fallback={<CircularProgress/>}>
-                  <img id="project_img" src={ML_img} alt="ML"></img>
+                  <Suspense fallback={<CircularProgress />}>
+                    <img id="project_img" src={ML_img} alt="ML"></img>
                   </Suspense>
                 </a>
               }
@@ -110,21 +125,52 @@ class Projects extends React.Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <Suspense fallback={<CircularProgress/>}>
-                  <img id="project_img" src={project_C_img} alt="Artic"></img>
+                  <Suspense fallback={<CircularProgress />}>
+                    <img id="project_img" src={project_C_img} alt="Artic"></img>
                   </Suspense>
                 </a>
               }
               description={project_C}
             />
 
+            <Project
+              title={
+                <a
+                  id="link"
+                  href="https://fitness-pall.web.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Suspense fallback={<CircularProgress />}>
+                    <img id="project_img" src={base_logo} alt="Fitness-Pall" />
+                  </Suspense>
+                </a>
+              }
+              description={fitness_pall}
+            />
+
+            <Project
+              title={
+                <a
+                  id="link"
+                  href="https://ha-pictures.web.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Suspense fallback={<CircularProgress />}>
+                    <img id="project_img" src={base_logo} alt="Ha-pictures" />
+                  </Suspense>
+                </a>
+              }
+              description={ha_pictures}
+            />
             {/* Nieuwe project
             <Project title={} description={} />*/}
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Projects;
+export default Projects
